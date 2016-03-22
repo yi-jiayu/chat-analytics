6de32faa4f2b886115e120c6b423dd78950fb129
Robin Lee <robin.lee.jw@gmail.com>
2016-03-23 03:28:14 +0800

    Added server.js for live-server

---
+++ server.js
@@ -0,0 +1,14 @@
+var liveServer = require("live-server");
+
+var params = {
+    port: 8181, // Set the server port. Defaults to 8080.
+    host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0.
+    root: "", // Set root directory that's being server. Defaults to cwd.
+    open: false, // When false, it won't load your browser by default.
+    ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+    file: "demo/index.html", // When set, serve this file for every 404 (useful for single-page applications)
+    wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+    mount: [['/components', './node_modules']], // Mount a directory to a route.
+    logLevel: 2 // 0 = errors only, 1 = some, 2 = lots
+};
+liveServer.start(params);
