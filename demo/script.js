a6a9f7fdc6102b4198141b65ae7c10d09eca4c61
jiayu <yijiayu@gmail.com>
2016-03-23 12:08:12 +0800

    Refactored demo page to use custom PunchCard chart

--- demo/script.js
+++ demo/script.js
@@ -3,9 +3,11 @@
 var messages = [];
 var timestamps = [];
 var dataArray = [];
-var googleDataArray = [['ID', 'Hour', 'Day', 'Series', 'Messages'], ['0', 0, 0, 'no', 0]];
-
-const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var googleDataArray = [[
+    {label: 'timestamp', type: 'datetime'},
+    {label: 'sender', type: 'string'},
+    {label: 'message', type: 'string'}]
+];
 
 // Check for the various File API support.
 if (window.File && window.FileReader && window.FileList && window.Blob) {
@@ -29,16 +31,6 @@ class WhatsAppTimestamp {
         if (ampm && /PM|pm/.test(match[6])) hour += 12;
 
         this.date = new Date(year, month, day, hour, minutes);
-        this.day = this.date.getDay();
-        this.hour = this.date.getHours();
-    }
-}
-
-class WhatsAppMessage {
-    constructor(match) {
-        this.date = new WhatsAppTimestamp(match[1]).date;
-        this.sender = match[2];
-        this.text = match[3];
     }
 }
 
@@ -50,7 +42,6 @@ function handleFileSelect(evt) {
     messages = [];
     timestamps = [];
     dataArray = [];
-    googleDataArray = [['ID', 'Hour', 'Day', 'Series', 'Messages']];
 
     reader.readAsText(chatHistory);
 
@@ -61,41 +52,15 @@ function handleFileSelect(evt) {
         while (match != null) {
             match = re.exec(event.target.result);
             if (match != null) {
-                messages.push(new WhatsAppMessage(match));
-                timestamps.push(new WhatsAppTimestamp(match[1]));
+                googleDataArray.push([new WhatsAppTimestamp(match[1]).date, match[2], match[3]]);
             }
         }
 
-        if (messages.length == 0) {
+        if (googleDataArray.length == 1) {
             alert('Oops, it looks like this file is probably not a whatsapp conversation.');
             return;
         }
 
-        initialiseDataArray();
-        populateDataArray();
-        formatDataArray();
-        drawSeriesChart(`${evt.target.files[0].name.replace('.txt', '')} (${messages.length + 1} messages)`);
+        drawSeriesChart(`${evt.target.files[0].name.replace('.txt', '')} (${googleDataArray.length - 1} messages)`);
     };
-}
-
-function initialiseDataArray() {
-    for (var i = 0; i < 7; i++) {
-        for (var j = 0; j < 24; j++) {
-            dataArray.push([i, j, 0]);
-        }
-    }
-}
-
-function populateDataArray() {
-    for (var i = 0; i < timestamps.length; i++) {
-        var obj = timestamps[i];
-        dataArray[obj.day * 24 + obj.hour][2]++;
-    }
-}
-
-function formatDataArray() {
-    for (var i = 0; i < dataArray.length; i++) {
-        var obj = dataArray[i];
-        googleDataArray.push(['' + DAYS[obj[0]] + ' ' + timeTicks[obj[1]].f, obj[1], 6 - obj[0], '', obj[2]]);
-    }
 }
